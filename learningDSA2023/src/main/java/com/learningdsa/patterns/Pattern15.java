f789c3e07f3e49cf158adbb69d0bb83eddd29292
tejas <tejas@tejass-MacBook-Pro.local>
2022-12-21 16:21:51 +0530

    day 2 21th dec

---
+++ learningDSA2023/src/main/java/com/learningdsa/patterns/Pattern15.java
@@ -0,0 +1,46 @@
+package com.learningdsa.patterns;
+
+import java.util.Scanner;
+
+public class Pattern15 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt();
+        int os =n/2;
+        int st=1;
+        int val=1;
+        for(int i=1;i<=n;i++) {
+
+            for(int j=1;j<=os;j++)
+            {
+                System.out.print("\t");
+            }
+            int cval=val;
+            for(int j=1;j<=st;j++)
+            {
+                System.out.print(cval +"\t");
+                if(j<=st/2)
+                {
+                    cval++;
+                }
+                else
+                {
+                    cval--;
+                }
+            }
+            if(i<=n/2)
+            {
+                os--;
+                st=st+2;
+                val++;
+            }
+            else
+            {
+                os++;
+                st=st-2;
+                val--;
+            }
+            System.out.println();
+        }
+        }
+}
