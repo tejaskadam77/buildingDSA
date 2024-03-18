1ffa86c8f2c73849f092ff3796f57307909bbf11
tejas <tejaskadam77@gmail.com>
2024-03-18 23:18:34 +0530

    day 3 daily commit

--- learningDSA2023/src/main/java/com/learningdsa/striver/linkedList/LList.java
+++ learningDSA2023/src/main/java/com/learningdsa/striver/linkedList/LList.java
@@ -96,6 +96,25 @@ public class LList {
         }
         return prev;
     }
+    public ListNode oddEvenList(ListNode head) {
+        if(head==null || head.next==null)
+            return head;
+        ListNode odd=head;
+        ListNode even=head.next;
+        ListNode evenHead=head.next;
+
+        while(even!=null && even.next!=null)
+        {
+            odd.next=odd.next.next;
+            even.next=even.next.next;
+            odd=odd.next;
+            even=even.next;
+        }
+        odd.next=evenHead;
+        return head;
+
+
+    }
     public static boolean isPalindrome(ListNode head) {
         ListNode slow=head;
         ListNode fast=head;
