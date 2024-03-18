5d9e722fac21668524774157d33a204bcb841f63
tejas <tejaskadam77@gmail.com>
2024-03-18 23:51:07 +0530

    day 3 daily commit update

--- learningDSA2023/src/main/java/com/learningdsa/striver/linkedList/LList.java
+++ learningDSA2023/src/main/java/com/learningdsa/striver/linkedList/LList.java
@@ -148,5 +148,28 @@ print(second);
         return true;
 
     }
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+
+        ListNode fast=head;
+        ListNode slow=head;
+        int i=0;
+        while( i<n)
+        {
+            fast=fast.next;
+            i++;
+        }
+        if(fast==null)
+        {
+            return head.next;
+        }
+        while(fast.next!=null)
+        {
+            slow=slow.next;
+            fast=fast.next;
+        }
+
+        slow.next=slow.next.next;
+        return head;
+    }
 
 }
